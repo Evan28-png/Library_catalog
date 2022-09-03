d1976cb6c0c2d70399bed6258027fa9713d99753
Evan Pierre <evanpierre23@gmail.com>
2022-09-03 13:04:00 +0300

    Final commit

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class Library:
         return self.members.insert_member(member)
 
     def get_members(self):
-        pass
+        return self.members
 
     def get_books(self):
         return self.books
@@ -53,16 +53,36 @@ class Library:
             else:
                 return 'Error Book not found'
 
+    def remove_book(self, book_id):
+        search_book = self.books.search(book_id)
+        if search_book:
+            index = self.books.index(search_book)
+            self.books.pop(index)
+            return f'{search_book.title} REMOVED'
+        else:
+            return 'Book doesnt exist'
+    
+    def print_transactions(self, id_no, month, date):
+        search_member = self.members.search(id_no)
+        if search_member:
+            return search_member.get_transactions(month, date)
+        else:
+            return 'member not found'
 
+    def search_member(self, id_no):
+        return self.members.search(id_no)
 
             
+
 class Memberlist(list):
     def search(self, id_no):
-        for member in self:
-            if str(member.id_no) == str(id_no):
-                return member
-            else:
-                return None
+        result = [obj for obj in self if str(obj.id_no) == str(id_no)]
+        member = result[0]
+        if member:
+            return member
+        else:
+            return None
+
 
     def insert_member(self, member):
         self.append(member)
@@ -71,11 +91,12 @@ class Memberlist(list):
 
 class Catalog(list):
     def search(self, id_no):
-        for book in self:
-            if str(book.id_no) == str(id_no):
-                return book
-            else:
-                return None
+        result = [obj for obj in self if str(obj.id_no) == str(id_no)]
+        book = result[0]
+        if book:
+            return book
+        else:
+            return None
 
     def get_books(self):
         return self
@@ -95,14 +116,31 @@ class Member:
         self.books_issued = []
 
     def issue_book(self, book):
+        date = datetime.date.today()
+        content = f'Issued with book {book.title.upper()}'
+        trans = Transaction(date, content)
+        self.transactions.append(trans)
         return book.issue(self)
 
     def get_booksissued(self):
         return self.books_issued
 
     def return_book(self, book):
+        date = datetime.date.today()
+        content = f'Retured book {book.title.upper()}'
+        trans = Transaction(date, content)
+        self.transactions.append(trans)
         return book.return_book(self)
 
+    def get_transactions(self, month, date):
+        result = [obj for obj in self.transactions if str(obj.date.month) == str(month) and str(obj.date.day) == str(date)]
+        if not result:
+            print('No transactions on that date')
+        else:
+            count = 1
+            for a in result:
+                print(f'{count}. {a.transactions}')
+                count = count + 1 
 
 
 class Book:
@@ -120,9 +158,9 @@ class Book:
             self.borrower = member
             while True:
                 try:
-                    days = int(input('Enter no of days'))
+                    days = int(input('Enter no of days: '))
                     if days > 14:
-                        print('No of days should be 14 or less days')
+                        print('No of days should be 14 or less')
                         continue
                 except ValueError:
                     continue
@@ -138,4 +176,8 @@ class Book:
         member.books_issued.clear()
         return 'Book has been returned'
 
+class Transaction:
+    def __init__(self, date=None, transactions=None):
+        self.date = date
+        self.transactions = transactions
 
--- ui.py
+++ ui.py
@@ -1,7 +1,13 @@
+from main import Library
+import sys
+
 class Interface:
+    def __init__(self):
+        self.library = Library()
+        self.choice = {'1': self.register_member, '2': self.add_books, '3': self.issue_book, '4': self.return_book, '7': self.remove_book, '8': self.print_transaction, '9': self.all_members, '10': self.all_books, '11': self.search_member, '0': self.exit}
+
     def display(self):
-        while True:
-            print("""
+        print("""
                     LIBRARY MENU
                     ========================
                     1. Register member
@@ -10,33 +16,80 @@ class Interface:
                     4. Return book
                     7. Remove book
                     8. Print transaction
-                    9. Place hold
-                    10.Remove hold
-                    11.Renew book
-                    12.Notify availability
+                    9. All members
+                    10. All books
+                    11. Search member
                     """)
 
+    def run(self):
+        self.display()
+        while True:
+            choice = input('Enter choice:\n')
+            if choice:
+                action = self.choice.get(choice)
+                if action:
+                    action()
+
+
+
     def register_member(self):
-        pass
+        while True:
+            name = input('Enter your name: ')
+            address = input('Enter your address: ')
+            id_no = input('Enter your id no: ')
+            print(self.library.add_member(name, id_no, address))
+            choice = input('Do you want to add another:(YES/NO) ')
+            if choice.lower() == 'yes':
+                continue
+            else:
+                break
+
     def add_books(self):
-        pass
+        while True:
+            title = input('Enter book title: ')
+            author = input('Enter book author: ')
+            id_no = input('Enter book id no : ')
+            print(self.library.add_book(title, author, id_no))
+            choice = input('Do you want to add more books:(YES/NO) ')
+            if choice.lower() == 'yes':
+                continue
+            else:
+                break
+             
     def issue_book(self):
-        pass
+        id_no = input('Enter member id no: ')
+        print(self.library.issue_book(id_no))
+
+
     def return_book(self):
-        pass
+        member = input('Enter member id')
+        print(self.library.return_book(member))
+
     def remove_book(self):
-        pass
+        book_id = input('Enter book id')
+        return self.library.remove_book(book_id)
+
     def print_transaction(self):
-        pass
-    def place_hold(self):
-        pass
-    def remove_hold(self):
-        pass
-    def renew_book(self):
-        pass
-    def notify_availability(self):
-        pass
+        id_no = input('Enter member id no: ')
+        month = input('Enter month: ')
+        date = input('Enter date of month: ')
+        print(self.library.print_transactions(id_no, month, date))
+
+    def all_members(self):
+        print(self.library.get_members())
+
+    def all_books(self):
+        print(self.library.get_books())
+
+    def search_member(self):
+        id_no = input('Enter id no: ')
+        print(self.library.search_member(id_no))
+
+    def exit(self):
+        print('Exiting............')
+        sys.exit()
 
 
-i = Interface()
-i.display()
+if __name__ == '__main__':
+    ui = Interface()
+    ui.run()
